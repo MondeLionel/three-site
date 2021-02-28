bb745ce97d73c30d4363685ac8d682acdf392d38
Monde Nyawose <lionelnyawose@gmail.com>
2021-02-28 19:28:22 +0200

    added sound from howler api

--- js/main.js
+++ js/main.js
@@ -1,27 +1,74 @@
-(function(window,document){
+import snow from '../media/audio/snow.mp3';
 
+(function(window,document,howler){
 
+let body = document.querySelector("body");
+
+Howler.volume(0.1)
 
 window.addEventListener("load", (event)=>{
-let confirm = document.querySelector(".confirm");
+	let confirm = document.querySelector(".confirm");
+	confirm.classList.remove("hidden");
 
-confirm.classList.remove("hidden")
+
 })
 
 let confirmBtn = document.querySelector("#loaded");
 let rejectBtn = document.querySelector("#reject");
 let loader = document.querySelector(".loader");
-
+// let body = document.querySelector("body");
 
 confirmBtn.addEventListener("click", allowMusic, false);
 rejectBtn.addEventListener("click", noMusic, false);
 
 
 function allowMusic(){
-	loader.style.display="none"
-}
+	body.classList.remove('loading')
+	// loader.style.display="none";
+	body.classList.add("music-playing");
+	body.classList.remove("music-stopped");
+	setTimeout(function(){
+		const id1 = snowy.play();
+		snowy.fade(0.1,0.9,0.5,2000,id1);
+		},1500)
+	}
 
 function noMusic(){
-	console.log("removed music")
-}
-})(window,document)
+	body.classList.remove('loading')
+	body.classList.remove("music-playing");
+	body.classList.add("music-stopped");
+	console.log("removed music");
+	// loader.style.display="none";
+	}
+
+
+var snowy = new Howl({
+  src: [snow],
+  onload: function(){
+  	console.log("loading");
+  },
+  onplay: function(){
+  	console.log("playing")
+  },
+  onloaderror: function(err){
+  	console.log(err);
+  },
+  onend: function() {
+    console.log('Finished!');
+  },
+  onfade:function(){
+  	console.log("fading");
+  },
+  onplayerror: function() {
+    snowy.once('unlock', function() {
+      sound.play();
+    });
+  }
+});
+
+
+
+
+
+
+})(window,document,Howler)
